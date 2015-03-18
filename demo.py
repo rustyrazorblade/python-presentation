c47c7102690486978bcb1eed3247d8bb016cc6d7
Jon Haddad <jon@jonhaddad.com>
2015-03-18 12:02:00 -0700

    updated for talks

--- demo.py
+++ demo.py
@@ -2,11 +2,34 @@ from flask import Flask
 app = Flask(__name__)
 
 # define our models
+class User(Model):
+    user_id = UUID(primary_key=True)
+    name = Text()
+    email = Text(required=True)
 
-# configure cassandra
+class EmailToUser(Model):
+    email = Text(primary_key=True)
+    user_id = UUID(required=True)
 
-# define a route
+class LoginHistory(Model):
+    # TTL = 60 days
+    user_id = UUID(primary_key=True)
+    login_date = DateTime(primary_true=True)
+    ip_address = Text()
+    
+# routes
+
+@app.before_first_request
+def connect_to_db():
+    print "Connecting to cassandra"
+    try:
+        setup(["localhost"], "users_demo")
+    except Exception as e:
+        print e
+
+# routes
 
 
 print("Starting up the demo app")
 app.run(port=7777)
+
